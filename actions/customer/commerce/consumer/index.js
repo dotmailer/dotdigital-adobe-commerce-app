79d41660dce483f66ce0161e4a998fd6e885e989
Shaun Hogan <Shaun.Hogan@dotdigital.com>
2025-02-21 14:38:33 +0000

    Merged PR 62510: Customer Consumer
    ## What's being changed
    We have added event subscriptions and actions to handle a new contact creation or update when a contact model is updated in Magento Commerce.
    
    ## How to review / test this change
    
    #### Local
    - Pull the latest code.
    - Update your environment with relevant values via the .env.dist.0
    - Run `npm run commerce-event-subscribe`.
    - Run `npm run onboard`.
    - Test the https://localhost:9080/api/v1/web/customer-commerce/consumer via Postman (see notes for payload) via the `aio app dev`
    - Observe the return value payload, cross-check values from the response with what you expected.
    - Trigger an observable event_data entry on Magento by creating or modifying a customer.
    - Run cron or wait for cron to pick up the new event entry.
    - Observe the update in Dotdigital.
    
    #### Adobe Test
    - Update your environment with relevant values via the .env.dist.0
    - Ensure you have Ngrok setup see https://dashboard.ngrok.com/get-started/setup/windows or https://dashboard.ngrok.com/get-started/setup/macos its really easy.
    - After setting up ngrok run the following command `ngrok http --host-header=rewrite my-local-instance:443 ` leave this running
    - Update the .env with the ngrok URL IE:  `https://494d-156-155-97-8.ngrok-free.app`
    - Run `aio app deploy`
    - Run `npm run commerce-event-subscribe`.
    - Run `npm run onboard`.
    - Trigger the event on Adobe commerce by creating a new contact and then adding address to the Customer
    
    #### Adobe Postman test
    - Run `aio app get-url` get your consumer url
    - in postman use the aforementioned URL you retrieved from the `aio app get-url`
    - run `aio login`  copy the token to your postman auth as a Bearer Token
    - get your `x-gw-ims-org-id` form the workspace config  `project.org.ims_org_id` and add it to the headers of your postman `x-gw-ims-org-id: xxxxxxxxxx`
    - open the adobe console navigate to your project and current workspace
    -  On the left click **events** > { event in question }  click debug and tracing tab
    - Copy the `payload` object form the event adobe triggered for this event see below **NOTES**: example
    - Paste this in your payload you copied to the raw body for you request.
    - Insure your request is set to pose and Send
    - you should receive a response with ` { "message": "Contact created successfully" }`
    - Observer your contact creation or update in the dotdigital platform
    
    ## NOTES
    Afforementioned payload.
    
    ```
    {
        "data": {
            "key": "883f8297-c325-4296-a738-e61b165bed10",
            "value": {
                "id": 2,
                "email": "shaun.hogan@dotdigital.com"
            },
            "source": "Dotdigital.Stage",
            "_metadata": {
                "commerceEdition": "Adobe Commerce",
                "commerceVersion": "2.4.7-p3",
                "eventsClientVersion": "1.11.0",
                "storeId": "1",
                "websiteId": "1",
                "storeGroupId": "1"
            }
        },
        "id": "cea550c9-16bb-4131-9ab4-5b099b63bece",
        "source": "urn:uuid:5942b48d-56f2-4aa2-b2b4-8d6af00397d0",
        "spec...

---
+++ actions/customer/commerce/consumer/index.js
@@ -0,0 +1,163 @@
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, checkMissingRequestInputs } = require('../../../utils')
+const { CommerceApi, DotdigitalApi } = require('../../../../lib')
+
+/**
+ * Class representing the handler for patching contacts.
+ */
+class ContactPatchHandler {
+  /**
+   * Create a ContactPatchHandler.
+   * @param {object} params - The parameters for the handler.
+   */
+  constructor (params) {
+    this.logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+
+    this.commerceApi = new CommerceApi({
+      url: params.COMMERCE_BASE_URL,
+      consumerKey: params.COMMERCE_CONSUMER_KEY,
+      consumerSecret: params.COMMERCE_CONSUMER_SECRET,
+      accessToken: params.COMMERCE_ACCESS_TOKEN,
+      accessTokenSecret: params.COMMERCE_ACCESS_TOKEN_SECRET
+    }, this.logger)
+    this.dotdigitalApi = new DotdigitalApi(
+      params.DOTDIGITAL_API_URL,
+      params.DOTDIGITAL_API_USER,
+      params.DOTDIGITAL_API_PASSWORD,
+      this.logger
+    )
+  }
+
+  /**
+   * Static method to invoke the main function.
+   * @param {object} params - The parameters for the handler.
+   * @returns {Promise<object>} The result of the main function.
+   */
+  static async invoke (params) {
+    const handler = new ContactPatchHandler(params)
+    return handler.main(params)
+  }
+
+  /**
+   * Map contact data fields from customer data.
+   * @param {object} dataFieldMapping - The mapping of data fields.
+   * @param {object} customerData - The customer data.
+   * @returns {Promise<object>} The mapped contact data fields.
+   * @throws Will throw an error if mapping fails.
+   */
+  async mapContactDataFields (dataFieldMapping, customerData) {
+    const dotdigitalDataFields = await this.dotdigitalApi.getContactDataFields()
+    const allowedDataFields = dotdigitalDataFields.filter(
+      dataField => Object.keys(dataFieldMapping).includes(dataField.name)
+    ).reduce((acc, dataField) => {
+      dataField.mapping = dataFieldMapping[dataField.name]
+      acc.push(dataField)
+      return acc
+    }, [])
+
+    const commerceCustomer = await this.commerceApi.getCustomer(customerData.id)
+    const customer = Object.assign(commerceCustomer, customerData)
+    const storeViews = await this.commerceApi.getStoreViews()
+    const websites = await this.commerceApi.getWebsites()
+    const customerGroup = await this.commerceApi.getCustomerGroup(customer.group_id)
+
+    storeViews.forEach(storeView => {
+      if (storeView.id === customer.store_id) {
+        customer.store_name = storeView.name
+      }
+    })
+
+    websites.forEach(website => {
+      if (website.id === customer.website_id) {
+        customer.website_name = website.name
+      }
+    })
+
+    customer.billing_address = commerceCustomer.addresses.find(
+      address => address.id === parseInt(commerceCustomer.default_billing)
+    ) ?? {}
+
+    customer.shipping_address = commerceCustomer.addresses.find(
+      address => address.id === parseInt(commerceCustomer.default_shipping)
+    ) ?? {}
+
+    if (customer.extension_attributes?.is_subscribed) {
+      customer.extension_attributes.is_subscribed = 'Subscribed'
+    } else {
+      delete customer.extension_attributes.is_subscribed
+    }
+
+    if (customerGroup) {
+      customer.group = customerGroup.code
+    }
+
+    delete customer.default_billing
+    delete customer.default_shipping
+    delete customer.addresses
+    delete customer.website_id
+    delete customer.store_id
+
+    return allowedDataFields.reduce((acc, dataField) => {
+      if (dataField.mapping.split('.').length > 1) {
+        const [parent, child, index] = dataField.mapping.split('.')
+        if (customer[parent] && customer[parent][child]) {
+          if (index) {
+            acc[dataField.name] = customer[parent][child][index]
+          } else {
+            acc[dataField.name] = customer[parent][child]
+          }
+        }
+        return acc
+      }
+      if (customer[dataField.mapping]) {
+        acc[dataField.name] = customer[dataField.mapping]
+      }
+      return acc
+    }, {})
+  }
+
+  /**
+   * Main function to create or update a contact in Dotdigital.
+   * @param {object} params - The parameters for the handler.
+   * @returns {Promise<object>} The result of the contact creation or update.
+   */
+  async main (params) {
+    try {
+      const dataFieldMapping = JSON.parse(params.DOTDIGITAL_DATAFIELD_MAPPING)
+      const customerData = params.data.value
+      const requiredParams = ['id', 'email']
+      const errorMessage = checkMissingRequestInputs(customerData, requiredParams)
+      if (errorMessage) {
+        return errorResponse(400, errorMessage + JSON.stringify(customerData), this.logger)
+      }
+
+      const contact = {
+        matchIdentifier: 'email',
+        identifiers: {
+          email: customerData.email
+        },
+        dataFields: await this.mapContactDataFields(dataFieldMapping, customerData),
+        lists: customerData.lists || [Number(params.DOTDIGITAL_LIST_ID)]
+      }
+
+      const dotdigitalContact = await this.dotdigitalApi.patchContactByEmail(customerData.email, contact)
+
+      return {
+        body: {
+          message: 'Contact created successfully',
+          contact: dotdigitalContact
+        }
+      }
+    } catch (error) {
+      this.logger.error(error)
+      return errorResponse(error?.status ?? 500, error.message, this.logger)
+    }
+  }
+}
+
+/**
+ * Export the main function to be executed by Adobe I/O Runtime.
+ * @param {object} params - The parameters for the handler.
+ * @returns {Promise<object>} The result of the main function.
+ */
+exports.main = (params) => ContactPatchHandler.invoke(params)
--- actions/utils.js
+++ actions/utils.js
@@ -108,7 +108,34 @@ function checkMissingRequestInputs (
   return errorMessage
 }
 
+/**
+ *
+ * Returns an error response object and attempts to log.info the status code and error message
+ *
+ * @param {number} statusCode the error status code.
+ *        e.g. 400
+ * @param {string} message the error message.
+ *        e.g. 'missing xyz parameter'
+ * @param {*} [logger] an optional logger instance object with an `info` method
+ *        e.g. `new require('@adobe/aio-sdk').Core.Logger('name')`
+ * @returns {object} the error object, ready to be returned from the action main's function.
+ */
+function errorResponse (statusCode, message, logger) {
+  if (logger && typeof logger.info === 'function') {
+    logger.info(`${statusCode}: ${message}`)
+  }
+  return {
+    error: {
+      statusCode,
+      body: {
+        error: message
+      }
+    }
+  }
+}
+
 module.exports = {
   stringParameters,
-  checkMissingRequestInputs
+  checkMissingRequestInputs,
+  errorResponse
 }
