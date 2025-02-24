9c242290003a5623a72e57c7ff43cce2924ac89c
Gustaf Landman <Gustaf.Landman@dotdigital.com>
2025-02-24 12:19:06 +0000

    Merged PR 62573: Product Consumer
    ## What's being changed
    We have added event subscriptions and actions to handle a new product creation or update when a product model is updated in Magento Commerce.
    
    ## How to review / test this change
    - Pull the latest code.
    - Update your environment with relevant values via the .env.dist.
    - Add DOTDIGITAL_CATALOG_COLLECTION_NAME=Catalog_base_default to env
    - Run `npm run commerce-event-subscribe`.
    - Run `npm run onboard`.
    - Test the https://localhost:9080/api/v1/web/product-commerce/consumer  via Postman (see notes for payload) via the `aio app dev`
    - Observe the return value payload, cross-check values from the response with what you expected.
    - Observe the update in Dotdigital.
    
    Related work items: #287080

---
+++ actions/product/commerce/consumer/index.js
@@ -0,0 +1,108 @@
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, checkMissingRequestInputs } = require('../../../utils')
+const { CommerceApi, DotdigitalApi } = require('../../../../lib')
+
+class ProductConsumer {
+  /**
+   * Create a ProductConsumer.
+   * @param {object} params - The parameters for the handler.
+   */
+  constructor (params) {
+    this.logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+    this.commerceApi = new CommerceApi({
+      url: params.COMMERCE_BASE_URL,
+      consumerKey: params.COMMERCE_CONSUMER_KEY,
+      consumerSecret: params.COMMERCE_CONSUMER_SECRET,
+      accessToken: params.COMMERCE_ACCESS_TOKEN,
+      accessTokenSecret: params.COMMERCE_ACCESS_TOKEN_SECRET
+    }, this.logger)
+    this.dotdigitalApi = new DotdigitalApi(
+      params.DOTDIGITAL_API_URL,
+      params.DOTDIGITAL_API_USER,
+      params.DOTDIGITAL_API_PASSWORD,
+      this.logger
+    )
+  }
+
+  /**
+   * Static method to invoke the main function.
+   * @param {object} params - The parameters for the handler.
+   * @returns {Promise<object>} The result of the main function.
+   */
+  static async invoke (params) {
+    const handler = new ProductConsumer(params)
+    return handler.main(params)
+  }
+
+  /**
+   * This is the consumer of the events coming from Adobe Commerce related to the product entity.
+   *
+   * @returns {object} returns response object with status code, request data received and response of the invoked action
+   * @param {object} params - includes the env params, type and the data of the event
+   */
+
+  async main (params) {
+    const returnObject = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: ''
+    }
+    try {
+      const product = params.data.value
+      const requiredParams = ['entity_id', 'name', 'sku', 'stock_data', 'price', 'status', 'type_id', 'url_key', 'image', 'created_at', 'store_ids']
+      const errorMessage = checkMissingRequestInputs(product, requiredParams)
+      if (errorMessage) {
+        return errorResponse(400, errorMessage + JSON.stringify(product), this.logger)
+      }
+
+      const requiredEnvParams = ['COMMERCE_BASE_URL', 'COMMERCE_CONSUMER_KEY', 'COMMERCE_CONSUMER_SECRET', 'COMMERCE_ACCESS_TOKEN', 'COMMERCE_ACCESS_TOKEN_SECRET', 'DOTDIGITAL_API_URL', 'DOTDIGITAL_API_USER', 'DOTDIGITAL_API_PASSWORD', 'DOTDIGITAL_CATALOG_COLLECTION_NAME']
+      const envErrorMessage = checkMissingRequestInputs(params, requiredEnvParams)
+      if (envErrorMessage) {
+        return errorResponse(400, envErrorMessage + JSON.stringify(params), this.logger)
+      }
+
+      const storeLinkUrl = await this.commerceApi.getStoreUrl(product.store_ids[0], 'link')
+      const storeMediaUrl = await this.commerceApi.getStoreUrl(product.store_ids[0], 'media')
+
+      // transform product payload
+      const productData = {
+        id: product.entity_id,
+        name: product.name,
+        type: product.type_id.charAt(0).toUpperCase() + product.type_id.slice(1),
+        status: (product.status === '1') ? 'Enabled' : 'Disabled',
+        stock: product.stock_data.qty,
+        sku: product.sku,
+        created_date: new Date(product.created_at).toISOString(),
+        price: product.price,
+        url: `${storeLinkUrl}${product.url_key}.html`,
+        imagePath: `${storeMediaUrl}catalog/product${product.image}`
+      }
+
+      if (product.parent_id) {
+        productData.parent_id = product.parent_id
+        productData.type = 'Variant'
+      }
+      const payload = {
+        Json: JSON.stringify(productData)
+      }
+
+      const response = await this.dotdigitalApi.postProductById(product.entity_id, params.DOTDIGITAL_CATALOG_COLLECTION_NAME, payload)
+
+      returnObject.statusCode = 200
+      returnObject.body = Buffer.from(JSON.stringify(response)).toString()
+    } catch (error) {
+      this.logger.error(error)
+      return errorResponse(error?.status ?? 500, error.message, this.logger)
+    }
+
+    return returnObject
+  }
+}
+/**
+ * Export the main function to be executed by Adobe I/O Runtime.
+ * @param {object} params - The parameters for the handler.
+ * @returns {Promise<object>} The result of the main function.
+ */
+exports.main = (params) => ProductConsumer.invoke(params)
